d11477e5a3f4cfb1bfe61272b362a72ff40c24a9
Yun Chung <yun.chung@mac.com>
2020-06-18 18:40:30 +0900

    Tested JS syntax and rake functionality

--- 00 - Le Wagon JS Review/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,5 +1,11 @@
 const sumOfNegative = (numbers) => {
   // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  // console.log("testing");
 };
+
+console.log(sumOfNegative([-4, 5, -2, 9]));
+
+
+// sumOfNegative(1);
 
 module.exports = sumOfNegative; // Do not remove.
