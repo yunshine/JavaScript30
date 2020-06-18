f7cb52f24c7e8ab07f946baed6bdacd2b6dd12df
Yun Chung <yun.chung@mac.com>
2020-06-18 19:01:55 +0900

    Completed Sum of Negative Challenge

--- 00 - Le Wagon JS Review/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,9 +1,20 @@
 const sumOfNegative = (numbers) => {
   // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
   // console.log("testing");
+  let sumArray = [];
+  let sumTotal = 0;
+  numbers.forEach((number) => {
+    if (number < 0) {
+      sumArray.push(number);;
+    }
+  })
+  sumArray.forEach ((number) => {
+    sumTotal = sumTotal + number;
+  })
+  return sumTotal;
 };
 
-console.log(sumOfNegative([-4, 5, -2, 9]));
+sumOfNegative([-4, 5, -2, 9]);
 
 
 // sumOfNegative(1);
