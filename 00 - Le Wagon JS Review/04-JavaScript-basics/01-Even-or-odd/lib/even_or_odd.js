b61e53dc0c6cbf00724bbb7c10f0d3f10ae7ac18
Yun Chung <yun.chung@mac.com>
2020-06-18 12:36:33 +0900

    Completed Even or Odd Challenge

--- 00 - Le Wagon JS Review/04-JavaScript-basics/01-Even-or-odd/lib/even_or_odd.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/01-Even-or-odd/lib/even_or_odd.js
@@ -1,5 +1,10 @@
 const evenOrOdd = (number) => {
   // TODO: this should return "even" if the number is even, "odd" otherwise
+  if (number % 2 == 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
 };
 
 module.exports = evenOrOdd; // Do not remove. We need this for the spec to know about your method.
