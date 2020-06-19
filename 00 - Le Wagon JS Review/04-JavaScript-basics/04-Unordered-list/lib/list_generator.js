14d3aecef10733fb8cb3d5a11f21fd0bb271f931
Yun Chung <yun.chung@mac.com>
2020-06-19 10:34:51 +0900

    Completed List Generator Challenge

--- 00 - Le Wagon JS Review/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,18 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  // => '<li class="list-group-item">milk</li>'
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
+  const listItems = []
   // TODO: return the proper <ul> markup (as a string)
+  items.forEach((item) => {
+    listItems.push(`<li class="list-group-item">${item}</li>`);
+  });
+  return `<ul class="list-group">${listItems.join('')}</ul>`;
 };
+
+console.log(unorderedList(['milk', 'butter', 'bread']));
 
 module.exports = { listItem, unorderedList }; // Do not remove.
