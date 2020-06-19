1e84d0752d5a64edbf2d99f4ca3aab186eb2178b
Yun Chung <yun.chung@mac.com>
2020-06-19 16:32:27 +0900

    Completed whoWinsTheWar function

--- 00 - Le Wagon JS Review/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -26,9 +26,23 @@ const buildSoldierObject = (battlefield) => {
 
 const whoWinsTheWar = (battlefield) => {
   // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
-  const keysValuesArray = Object.entries(buildSoldierObject(battlefield));
-  return keysValuesArray;
-  // Object.entries()
+  const keysValuesArray = Object.entries(buildSoldierObject(battlefield));  //=> [ [ 'Elves', 3 ], [ 'Hobbits', 7 ], [ 'Orcs', 2 ] ]
+  let numberOfGood = 0;
+  let numberOfEvil = 0;
+  keysValuesArray.forEach((soldierNumberArray) => {
+    if (soldierNumberArray[0] == "Hobbits" || soldierNumberArray[0] == "Elves" || soldierNumberArray[0] == "Dwarves" || soldierNumberArray[0] == "Eagles") {
+      numberOfGood = numberOfGood + (soldierNumberArray[1]);
+    } else {
+      numberOfEvil = numberOfEvil + (soldierNumberArray[1]);
+    }
+  });
+  if (numberOfGood > numberOfEvil) {
+    return "Good";
+  } else if (numberOfGood < numberOfEvil) {
+    return "Evil";
+  } else {
+    return "Tie";
+  }
 };
 
 
