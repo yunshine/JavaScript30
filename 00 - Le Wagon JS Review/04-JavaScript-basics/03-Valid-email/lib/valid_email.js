48e284a1373832c21933f10f229c0ccd0a3ac859
Yun Chung <yun.chung@mac.com>
2020-06-19 09:57:49 +0900

    Completed valid email challenge

--- 00 - Le Wagon JS Review/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,7 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const regCheck = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regCheck.test(email);
 };
 
 module.exports = valid; // Do not remove.
