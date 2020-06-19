87e1bc6eedf22f9474239ac8bbade41215b4036d
Yun Chung <yun.chung@mac.com>
2020-06-19 10:56:11 +0900

    Completed isGood function

--- 00 - Le Wagon JS Review/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 00 - Le Wagon JS Review/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -1,6 +1,11 @@
 const isGood = (soldierType) => {
   // TODO: return `true` if the soldierType is Good.
   // Hint: Hobbits, Elves, Dwarves and Eagles are Good
+  if (soldierType == "Hobbits" || soldierType == "Elves" || soldierType == "Dwarves" || soldierType == "Eagles") {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const buildSoldierObject = (battlefield) => {
@@ -10,5 +15,7 @@ const buildSoldierObject = (battlefield) => {
 const whoWinsTheWar = (battlefield) => {
   // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
 };
+
+console.log(isGood("Orcs"));
 
 module.exports = { whoWinsTheWar, buildSoldierObject, isGood }; // Do not remove this line
