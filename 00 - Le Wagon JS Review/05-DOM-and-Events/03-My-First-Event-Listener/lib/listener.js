776bcc399085ebb42346344ee58689d00c9f22ef
Yun Chung <yun.chung@mac.com>
2020-06-21 13:53:37 +0900

    Disabled button on click

--- 00 - Le Wagon JS Review/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 00 - Le Wagon JS Review/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,5 @@
 // TODO: React to a click on the button!
+const button = document.querySelector("#clickme");
+button.addEventListener("click", (event) => {
+  button.classList.add(".disabled");
+});
