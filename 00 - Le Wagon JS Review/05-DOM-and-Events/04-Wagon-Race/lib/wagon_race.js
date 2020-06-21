26c1ccd0cd58e5bfc9555ed7dff7ac7e0b5e4a0e
Yun Chung <yun.chung@mac.com>
2020-06-21 15:26:47 +0900

    Partially Completed Wagon Race Challenge

--- 00 - Le Wagon JS Review/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 00 - Le Wagon JS Review/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,8 @@
 // TODO: write your code here
+const playerOne = document.querySelector("#player1-race .active");
+
+playerOne.addEventListener("keyup", (event) => {
+  if ("keyup" == 81) {
+    playerOne.classList.remove("active");
+  }
+});
