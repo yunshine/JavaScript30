4730f02b18074ab4d8b15fa98e8c75a4816750af
Yun Chung <yun.chung@mac.com>
2020-06-21 13:57:48 +0900

    Button says plays music on click

--- 00 - Le Wagon JS Review/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 00 - Le Wagon JS Review/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,6 +1,8 @@
 // TODO: React to a click on the button!
 const button = document.querySelector("#clickme");
+const audio = new Audio('sound.mp3');
 button.addEventListener("click", (event) => {
   button.classList.add(".disabled");
   button.innerText = "Bingo!"
+  audio.play();
 });
