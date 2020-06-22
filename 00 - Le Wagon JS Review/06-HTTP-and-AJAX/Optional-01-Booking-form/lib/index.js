45f65cd79283cf56e64c38432ad7343cf93ebfad
Yun Chung <yun.chung@mac.com>
2020-06-22 12:34:41 +0900

    Added event listeners for plus/minus buttons

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
@@ -1,1 +1,9 @@
 // TODO: write your code here!
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const counter = Number.parseInt(document.querySelector("#counter").innerText);
+
+plus.addEventListener("click", (event) => {
+  console.log("plus has been clicked");
+  console.log(counter);
+});
