02623ebbda5371318b83aa6f0d65f65186e5bd8b
Yun Chung <yun.chung@mac.com>
2020-06-22 12:39:09 +0900

    Added counter functionality to plus button

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
@@ -1,9 +1,12 @@
 // TODO: write your code here!
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
-const counter = Number.parseInt(document.querySelector("#counter").innerText);
+const counter = document.querySelector("#counter");
+let counterTotal = 1;
 
 plus.addEventListener("click", (event) => {
   console.log("plus has been clicked");
-  console.log(counter);
+  counterTotal = counterTotal +1;
+  counter.innerText = counterTotal.toString();
+  console.log(counterTotal);
 });
