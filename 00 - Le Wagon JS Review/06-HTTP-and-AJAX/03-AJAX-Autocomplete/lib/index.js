748f7cd5d41c99b4eb4482b05fd47e83fa2b84e3
Yun Chung <yun.chung@mac.com>
2020-06-22 11:32:52 +0900

    Coded access to API

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,14 @@
 // TODO: Autocomplete the input with AJAX calls.
+const input = document.querySelector('#search');
+const results = document.querySelector('#results');
+
+const autocomplete = (query) => {
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query.value}`)
+  // fetch("https://wagon-dictionary.herokuapp.com/autocomplete/hat")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
+
+input.addEventListener('keyup', autocomplete);
