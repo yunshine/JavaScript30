3d1dc07edb766cce3f9e3696c059d8e48cbeacc1
Yun Chung <yun.chung@mac.com>
2020-06-22 11:40:19 +0900

    Connected API to event listener function

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -2,13 +2,22 @@
 const input = document.querySelector('#search');
 const results = document.querySelector('#results');
 
-const autocomplete = (query) => {
-  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query.value}`)
-  // fetch("https://wagon-dictionary.herokuapp.com/autocomplete/hat")
+// const autocomplete = (query) => {
+//   fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query.currentTarget.value}`)
+//   // fetch("https://wagon-dictionary.herokuapp.com/autocomplete/hat")
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
+
+// input.addEventListener('keyup', autocomplete);
+
+
+input.addEventListener("keyup", (event) => {
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${event.currentTarget.value}`)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
     });
 });
-
-input.addEventListener('keyup', autocomplete);
