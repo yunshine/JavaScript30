1b32cd203d41c8a4dcf0f568fd3352941750cb79
Yun Chung <yun.chung@mac.com>
2020-06-22 10:09:40 +0900

    Completed My Favourite Sport Challenge

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,8 @@
 // TODO: write your code here!
+const clickable = document.querySelectorAll(".clickable");
+
+clickable.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    element.classList.toggle("active");
+  });
+});
