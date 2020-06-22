61055166cd8569e214a958cea5ba655ec9a43904
Yun Chung <yun.chung@mac.com>
2020-06-22 18:37:09 +0900

    Connected Mapbox API to form

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
@@ -1,8 +1,14 @@
 // TODO: Write your JS code in here
-const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w"
+const submit = document.querySelector(".submitButton");
+const inputText = document.querySelector(".inputText");
 
-fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+
+submit.addEventListener("click", (event) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputText.value}.json?access_token=pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
