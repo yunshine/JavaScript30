db75b85642031040ab0b52df50e491071ce3a2f9
Yun Chung <yun.chung@mac.com>
2020-06-22 17:52:02 +0900

    Completed Bugfix challenge

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/01-Bugfix/lib/index.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/01-Bugfix/lib/index.js
@@ -1,2 +1,4 @@
 // TODO: find the bug and fix it!
+import { displayLogo } from './logo'; 
+
 displayLogo();
--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/01-Bugfix/lib/logo.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/01-Bugfix/lib/logo.js
@@ -2,3 +2,5 @@ const displayLogo = () => {
   const logo = document.querySelector('#logo');
   logo.style.display = '';
 };
+
+export { displayLogo };
