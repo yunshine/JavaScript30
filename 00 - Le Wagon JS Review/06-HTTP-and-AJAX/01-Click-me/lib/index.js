924d819acfa2d2c4c3125d9b11ccaeb75da11d47
Yun Chung <yun.chung@mac.com>
2020-06-22 09:52:33 +0900

    Completed Click Me Challenge

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/01-Click-me/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/01-Click-me/lib/index.js
@@ -1,6 +1,9 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  const button = document.querySelector(".btn");
+  button.addEventListener("click", (event) => {
+    window.alert("Thank you!");
+  });
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
 };
