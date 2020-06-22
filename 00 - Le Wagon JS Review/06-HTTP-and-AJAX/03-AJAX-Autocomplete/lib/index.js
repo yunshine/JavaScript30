bfb3e3151588b93198a6dfb2bdbe65baa79757b8
Yun Chung <yun.chung@mac.com>
2020-06-22 11:55:44 +0900

    AJAX Autocomplete is fully functional

--- 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 00 - Le Wagon JS Review/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -2,23 +2,12 @@
 const input = document.querySelector('#search');
 const results = document.querySelector('#results');
 
-// const autocomplete = (query) => {
-//   fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query.currentTarget.value}`)
-//   // fetch("https://wagon-dictionary.herokuapp.com/autocomplete/hat")
-//     .then(response => response.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// };
-
-// input.addEventListener('keyup', autocomplete);
-
-
 input.addEventListener("keyup", (event) => {
   fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${event.currentTarget.value}`)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
+      results.innerHTML = '';
       data.words.forEach((suggestion) => {
         console.log(suggestion);
         results.insertAdjacentHTML("beforeend", `<li>${suggestion}</li>`);
