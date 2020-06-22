7beeff925f9097d500de8ebe1f23125dec9f62d3
Yun Chung <yun.chung@mac.com>
2020-06-22 18:10:43 +0900

    Incorporated and tested Geocoder's use of Mapbox

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
@@ -1,1 +1,8 @@
 // TODO: Write your JS code in here
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w"
+
+fetch(url)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
