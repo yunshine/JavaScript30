2681c199ed2ea1cd37f288e0bbf1c28884d863ac
Yun Chung <yun.chung@mac.com>
2020-06-23 18:10:58 +0900

    Able to display the weather icon from the API

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
@@ -1,17 +1,14 @@
 // TODO: Write your JS code in here
 
-//   https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2&units=metric
-
-//   https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=daaf9d16ae4a8457c354919da42dd9e2&units=metric
-
 fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2&units=metric")
   .then(response => response.json())
   .then((data) => {
-    console.log(data);
+    console.log(data.weather[0].icon);
     const city = document.querySelector(".city");
     const dayDateTime = document.querySelector(".dayDateTime");
     const description = document.querySelector(".description");
     const tempNow = document.querySelector(".temp");
+    const weatherIcon = document.querySelector(".weatherIcon");
 
     const weekday = [];
     weekday[0] = "Sunday";
@@ -66,12 +63,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4
     city.innerText = `Weather in ${data.name}`;    
     description.innerText = `${data.weather[0].description}`;
     tempNow.innerText = `${data.main.temp}\u00B0C`;
-
-
-
-
-
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
 console.log(hour);
-
   });
