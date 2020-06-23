c4b9ac9ed9847d0ef65ceaa668f91853058e71ba
Yun Chung <yun.chung@mac.com>
2020-06-23 11:07:32 +0900

    Refactored code for displaying coordinates

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
@@ -1,31 +1,44 @@
-// TODO: Write your JS code in here
 import mapboxgl from 'mapbox-gl';
 
 const submit = document.querySelector(".submitButton");
 const inputText = document.querySelector(".inputText");
 
+const displayCoordinates = (coordinates) => {
+  const latitude = document.querySelector(".latitudeCoordinates");
+  const longitude = document.querySelector(".longitudeCoordinates");
+  latitude.innerText = `Latitude: ${coordinates.lat}`;
+  longitude.innerText = `Longitude: ${coordinates.long}`;
+};
+
 submit.addEventListener("click", (event) => {
+  // event.preventDefault();
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputText.value}.json?access_token=pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w`;
 
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      const latitude = document.querySelector(".latitudeCoordinates");
-      const longitude = document.querySelector(".longitudeCoordinates");
       const mapDisplay = document.querySelector('#map');
       mapboxgl.accessToken = 'pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w';
-      
+  
       console.log(data);
-      latitude.innerText = `Latitude: ${data.features[0].center[1]}`;
-      longitude.innerText = `Longitude: ${data.features[0].center[0]}`;
 
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
-        center: [ data.features[0].center[0], data.features[0].center[1] ],
-        zoom: 12
-      });
+      const coordinates = {
+        lat: data.features[0].center[1],
+        long: data.features[0].center[0],
+      };
+
+      displayCoordinates(coordinates);
+
+      // const map = new mapboxgl.Map({
+      //   container: 'map',
+      //   style: 'mapbox://styles/mapbox/streets-v9',
+      //   center: [ data.features[0].center[0], data.features[0].center[1] ],
+      //   zoom: 11,
+      // });
+
     });
 });
 
-
+        // new mapboxgl.Marker()
+        // .setLngLat([ data.features[0].center[0], data.features[0].center[1] ])
+        // .addTo(map);
