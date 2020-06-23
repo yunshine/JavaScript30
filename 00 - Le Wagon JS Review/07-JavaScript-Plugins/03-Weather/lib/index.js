b9d769a9455bc8af0a4600b62d3adc5f4bfc6872
Yun Chung <yun.chung@mac.com>
2020-06-23 14:52:04 +0900

    Displays day of week and month of year

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
@@ -12,11 +12,45 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4
     const dayDateTime = document.querySelector(".dayDateTime");
     const description = document.querySelector(".description");
     const tempNow = document.querySelector(".temp");
-    const now = new Date();   
-    console.log(now);
 
-    city.innerText = `Weather in ${data.name}`;
+    const weekday = [];
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    const months = [];
+    months[0] = "January";
+    months[1] = "February";
+    months[2] = "March";
+    months[3] = "April";
+    months[4] = "May";
+    months[5] = "June";
+    months[6] = "July";
+    months[7] = "August";
+    months[8] = "September";
+    months[9] = "October";
+    months[10] = "November";
+    months[11] = "December";
+
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const monthOfYear = today.getMonth();
+    const info = Date();
+
+    city.innerText = `Weather in ${data.name}`;    
+    dayDateTime.innerText = `${weekday[dayOfWeek]}, ${months[monthOfYear]}`;
     description.innerText = `${data.weather[0].description}`;
     tempNow.innerText = `${data.main.temp}\u00B0C`;
+
+
+
+
+
+
+console.log(weekday[dayOfWeek]);
 
   });
