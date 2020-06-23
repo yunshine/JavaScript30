848d8a9287e77c715808be41924d87749457176d
Yun Chung <yun.chung@mac.com>
2020-06-23 14:16:09 +0900

    Displayed city, temperature, and weather description from API

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
@@ -4,8 +4,19 @@
 
 //   https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=daaf9d16ae4a8457c354919da42dd9e2&units=metric
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2&units=metric")
   .then(response => response.json())
   .then((data) => {
     console.log(data);
+    const city = document.querySelector(".city");
+    const dayDateTime = document.querySelector(".dayDateTime");
+    const description = document.querySelector(".description");
+    const tempNow = document.querySelector(".temp");
+    const now = new Date();   
+    console.log(now);
+
+    city.innerText = `Weather in ${data.name}`;
+    description.innerText = `${data.weather[0].description}`;
+    tempNow.innerText = `${data.main.temp}\u00B0C`;
+
   });
