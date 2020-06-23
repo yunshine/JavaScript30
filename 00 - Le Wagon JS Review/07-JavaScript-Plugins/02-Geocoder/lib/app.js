b7f053b6b67a44ad2717919a24de443a0bb1c794
Yun Chung <yun.chung@mac.com>
2020-06-23 11:58:13 +0900

    Added a pin to the map and refactored mapDisplay function

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/02-Geocoder/lib/app.js
@@ -10,35 +10,40 @@ const displayCoordinates = (coordinates) => {
   longitude.innerText = `Longitude: ${coordinates.long}`;
 };
 
+const displayMap = (coordinates) => {
+  const lat = coordinates.lat;
+  const long = coordinates.long;
+  console.log(coordinates);
+  // const mapDisplay = document.querySelector('#map');
+  mapboxgl.accessToken = 'pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w';
+
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [ long, lat ],
+    zoom: 11,
+  });
+        new mapboxgl.Marker()
+        .setLngLat([ long, lat ])
+        .addTo(map);
+};
+
 submit.addEventListener("click", (event) => {
   // event.preventDefault();
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputText.value}.json?access_token=pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w`;
-
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      const mapDisplay = document.querySelector('#map');
-      mapboxgl.accessToken = 'pk.eyJ1IjoieXVuc2hpbmUiLCJhIjoiY2s5bnF1enNiMDU5ZTNvbnczM3k0aGIzaSJ9.KV8UR_F-hmPdVUBO-SWO9w';
   
       console.log(data);
 
-      const coordinates = {
+      let coordinates = {
         lat: data.features[0].center[1],
         long: data.features[0].center[0],
       };
 
       displayCoordinates(coordinates);
-
-      // const map = new mapboxgl.Map({
-      //   container: 'map',
-      //   style: 'mapbox://styles/mapbox/streets-v9',
-      //   center: [ data.features[0].center[0], data.features[0].center[1] ],
-      //   zoom: 11,
-      // });
+      displayMap(coordinates);
 
     });
 });
-
-        // new mapboxgl.Marker()
-        // .setLngLat([ data.features[0].center[0], data.features[0].center[1] ])
-        // .addTo(map);
