6da4f738399645c22ee9a2cc1316c0a265bc6719
Yun Chung <yun.chung@mac.com>
2020-06-23 12:35:16 +0900

    Coded access to the OpenWeatherMap API

--- 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
+++ 00 - Le Wagon JS Review/07-JavaScript-Plugins/03-Weather/lib/index.js
@@ -1,1 +1,11 @@
 // TODO: Write your JS code in here
+
+//   https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2
+
+//   https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=daaf9d16ae4a8457c354919da42dd9e2
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=daaf9d16ae4a8457c354919da42dd9e2")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
