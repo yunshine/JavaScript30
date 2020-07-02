0f9cc7d0aad46a11e1701aa26a26f80f79d95f78
Yun Chung <yun.chung@mac.com>
2020-07-02 11:03:28 +0900

    Added eventlisteners to the two sliders on the player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -21,9 +21,12 @@ function togglePlay() {
 }
 
 function skip() {
-  console.log(video);
   video.currentTime += parseFloat(this.dataset.skip);
 };
+
+function handleRangeUpdate() {
+  console.log(this.value);
+}
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -32,4 +35,8 @@ toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach((button) => {
   button.addEventListener("click", skip);
+});
+
+ranges.forEach((slider) => {
+  slider.addEventListener("change", handleRangeUpdate);
 });
