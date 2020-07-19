5c7420d563d03edb369d26acc71236e30b53b505
Yun Chung <yun.chung@mac.com>
2020-07-19 14:02:28 +0900

    Completed Countdown Timer challenge

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,14 +1,15 @@
 let countdown;
-
-function displayTimeLeft(seconds) {
-  console.log(seconds);
-}
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  displayTimeLeft(seconds);
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + (seconds * 1000);
-
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+  
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - (Date.now())) / 1000);
     if(secondsLeft < 0) {
@@ -16,6 +17,35 @@ function timer(seconds) {
       return;
     }
     displayTimeLeft(secondsLeft);
-}, 1000);
+  }, 1000);
 }
 
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const displayedTime = `${minutes}:${remainingSeconds < 10 ? '0' : '' }${remainingSeconds}`;
+  // console.log(displayedTime);
+  timerDisplay.textContent = displayedTime;
+  document.title = displayedTime;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Break ends at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  // console.log(seconds);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const mins = this.minutes.value;
+  this.reset();
+  timer(mins * 60);
+});
