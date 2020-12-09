8372ff482e0f99afe0694d90d8efe377aea4172c
Yun Chung <yun.chung@mac.com>
2020-12-09 12:26:08 +0900

    Timer Exercise Review - Countdown buttons and display are functional

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,51 +1,31 @@
-let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelectorAll('[data-time]');
-
-function timer(seconds) {
-  clearInterval(countdown);
-  const now = Date.now();
-  const then = now + (seconds * 1000);
-  displayTimeLeft(seconds);
-  displayEndTime(then);
-  
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - (Date.now())) / 1000);
-    if(secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
-    }
-    displayTimeLeft(secondsLeft);
-  }, 1000);
-}
-
-function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  const displayedTime = `${minutes}:${remainingSeconds < 10 ? '0' : '' }${remainingSeconds}`;
-  // console.log(displayedTime);
-  timerDisplay.textContent = displayedTime;
-  document.title = displayedTime;
-}
-
-function displayEndTime(timestamp) {
-  const end = new Date(timestamp);
-  const hour = end.getHours();
-  const minutes = end.getMinutes();
-  endTime.textContent = `Break ends at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
-}
+const buttons = document.querySelectorAll('.timer__button');
+const timeLeftDisplay = document.querySelector('.display__time-left');
+const timeEndDisplay = document.querySelector('.display__end-time');
 
 function startTimer() {
-  const seconds = parseInt(this.dataset.time);
-  // console.log(seconds);
-  timer(seconds);
+    const startTime = Date.now();
+    const endTime = Date.now() + parseInt(this.dataset.time);
+
+    timeLeftDisplay.innerText = this.dataset.time;
+    let secondsRemaining = parseInt(this.dataset.time);
+    const countdown = setInterval(function () {
+        secondsRemaining -= 1;
+        timeLeftDisplay.innerText = secondsRemaining;
+        if (secondsRemaining === 0) {
+            stopTimer();
+        };
+    }, 1000);
+
+    // console.log(secondsRemaining);
+
+
+    function stopTimer() {
+        clearInterval(countdown);
+    };
+
+    timeEndDisplay.innerText = this.dataset.time;
+    console.log(startTime, endTime);
+
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const mins = this.minutes.value;
-  this.reset();
-  timer(mins * 60);
-});
