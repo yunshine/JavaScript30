c5b6d668dd623376dfb2d3369ec475de8b48836c
Yun Chung <yun.chung@mac.com>
2020-12-10 16:18:26 +0900

    Able to set the timer using the form

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll('.timer__button');
 const timeLeftDisplay = document.querySelector('.display__time-left');
 const timeEndDisplay = document.querySelector('.display__end-time');
+const input = document.querySelector('form');
 
 function setTimerEndTime(seconds) {
     const newDate = new Date();
@@ -13,33 +14,45 @@ function setTimerEndTime(seconds) {
     const endMinutesToDisplay = (startMinute + (Math.floor((startSecond + seconds) / 60))) % 60;
     const endHoursToDisplay = (startHour + Math.floor((startMinute + (Math.floor((startSecond + seconds)) / 60)) / 60)) % 12;
 
-    timeEndDisplay.innerText = `${endHoursToDisplay}:${endMinutesToDisplay < 10 ? 0 : ""}${endMinutesToDisplay}:${endSecondsToDisplay < 10 ? 0 : ""}${endSecondsToDisplay}`;
+    timeEndDisplay.innerText = `This break ends at ${endHoursToDisplay}:${endMinutesToDisplay < 10 ? 0 : ""}${endMinutesToDisplay}:${endSecondsToDisplay < 10 ? 0 : ""}${endSecondsToDisplay}`;
 
 
     console.log("Seconds to Add: ", seconds);
     console.log("Hours NOW: ", newDate.getHours());
     console.log("Minutes NOW: ", newDate.getMinutes());
     console.log("Seconds NOW: ", newDate.getSeconds());
-    // console.log("Hours to Show: ", endHoursToDisplay);
-
-
 };
 
-function startTimer() {
+function buttonTimer() {
     let secondsRemaining = parseInt(this.dataset.time);
     setTimerEndTime(secondsRemaining);
-    timeLeftDisplay.innerText = `${this.dataset.time} ${this.dataset.time != 1 ? "seconds remaining" : "second remaining"}`;
+    timeLeftDisplay.innerText = `${secondsRemaining} ${secondsRemaining !== 1 ? "seconds remaining" : "second remaining"}`;
 
-    const countdown = setInterval(function () {
-        secondsRemaining -= 1;
-        timeLeftDisplay.innerText = `${secondsRemaining} ${secondsRemaining != 1 ? "seconds remaining" : "second remaining"}`;
-        if (secondsRemaining === 0) {
-            clearInterval(countdown);
-        };
-    }, 1000);
+    startCountdown(secondsRemaining);
 };
 
-buttons.forEach(button => button.addEventListener('click', startTimer));
+function startCountdown(secondsRemaining) {
+    const timer = setInterval(function () {
+        secondsRemaining -= 1;
+        timeLeftDisplay.innerText = `${secondsRemaining} ${secondsRemaining !== 1 ? "seconds remaining" : "second remaining"}`;
+        if (secondsRemaining === 0) {
+            clearInterval(timer);
+        };
+    }, 1000);
+}
+
+function inputTimer(e) {
+    e.preventDefault();
+    let formValue = document.querySelector('input').value;
+    let secondsRemaining = parseInt(formValue) * 60;
+    setTimerEndTime(secondsRemaining);
+    timeLeftDisplay.innerText = `${secondsRemaining} ${secondsRemaining !== 1 ? "seconds remaining" : "second remaining"}`;
+    startCountdown(secondsRemaining);
+
+}
+
+buttons.forEach(button => button.addEventListener('click', buttonTimer));
+input.addEventListener('submit', inputTimer);
 
 // function stopTimer() {
 //     clearInterval(countdown);
