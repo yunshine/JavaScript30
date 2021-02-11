efa0374accb8f139a570fcbeadd4e575adbcf994
Yun Chung <yun.chung@mac.com>
2021-02-11 18:44:03 +0900

    Game logic using if/else statements

--- 00 - Le Wagon JS Review/Misc. Tutorials/Rock, Scissors, Paper Game/app.js
+++ 00 - Le Wagon JS Review/Misc. Tutorials/Rock, Scissors, Paper Game/app.js
@@ -4,18 +4,53 @@ const humanChoiceImage = document.querySelector(".human-choice-image");
 const computerChoiceContainer = document.querySelector('.computer-choice-image-div');
 const computerChoiceImage = document.querySelector(".computer-choice-image");
 
-function showHumanChoice(evt) {
-    const choice = evt.target.innerText;
-    humanChoiceImage.setAttribute('src', `assets/${choice}.png`);
-    humanChoiceImage.setAttribute('alt', choice);
-    showComputerChoice();
+
+let currentHumanChoice = "";
+let currentComputerChoice = "";
+
+function draw() {
+    console.log("it's a draw");
+}
+
+function humanWins() {
+    console.log("humans win");
+}
+
+function computerWins() {
+    console.log("computers win");
+}
+
+function determineResult() {
+    if (currentHumanChoice === currentComputerChoice) {
+        draw();
+    } else if (currentHumanChoice === "rock" && currentComputerChoice === "scissors") {
+        humanWins();
+    } else if (currentHumanChoice === "scissors" && currentComputerChoice === "paper") {
+        humanWins();
+    } else if (currentHumanChoice === "paper" && currentComputerChoice === "rock") {
+        humanWins();
+    } else if (currentComputerChoice === "rock" && currentHumanChoice === "scissors") {
+        computerWins();
+    } else if (currentComputerChoice === "scissors" && currentHumanChoice === "paper") {
+        computerWins();
+    } else if (currentComputerChoice === "paper" && currentHumanChoice === "rock") {
+        computerWins();
+    }
 }
 
 function showComputerChoice(evt) {
     const computerChoices = ["rock", "scissors", "paper"];
-    const computerChoice = computerChoices[Math.floor(Math.random() * 3)];
-    computerChoiceImage.setAttribute('src', `assets/${computerChoice}.png`);
-    computerChoiceImage.setAttribute('alt', computerChoice);
+    currentComputerChoice = computerChoices[Math.floor(Math.random() * 3)];
+    computerChoiceImage.setAttribute('src', `assets/${currentComputerChoice}.png`);
+    computerChoiceImage.setAttribute('alt', currentComputerChoice);
+    determineResult();
+}
+
+function showHumanChoice(evt) {
+    currentHumanChoice = evt.target.innerText;
+    humanChoiceImage.setAttribute('src', `assets/${currentHumanChoice}.png`);
+    humanChoiceImage.setAttribute('alt', currentHumanChoice);
+    showComputerChoice();
 }
 
 buttons.forEach(button => {
