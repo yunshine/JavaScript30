89b1774274d7a093f37c2a97e7714e87a3692428
Yun Chung <yun.chung@mac.com>
2021-02-12 17:42:04 +0900

    Able to display first question on game start

--- 00 - Le Wagon JS Review/Misc. Tutorials/Random Exercises/Random Quiz Game/app.js
+++ 00 - Le Wagon JS Review/Misc. Tutorials/Random Exercises/Random Quiz Game/app.js
@@ -1,39 +1,47 @@
 const startSection = document.querySelector('.start-quiz-section');
 const startButton = document.querySelector('.start-quiz-button');
 const gameSection = document.querySelector('.game-section');
+const questionText = document.querySelector('#question');
+const choicesUL = document.querySelector('.choices');
 
 const questions = [
     {
-        question: ,
-        answer: ,
-        choices: [],
-    }
+        question: "What is Yun's favorite pizza restaurant?",
+        answer: "Savoy",
+        choices: ["La Strada", "Zachary's", "Savoy", "Little Caesar's"],
+    },
     {
-        question: ,
-        answer: ,
-        choices: [],
-    }
+        question: "What is Yun's favorite book?",
+        answer: "the Bible",
+        choices: ["the Bible", "Harry Potter", "The Lord of the Rings", "A Game of Thrones"],
+    },
     {
-        question: ,
-        answer: ,
-        choices: [],
-    }
-    {
-        question: ,
-        answer: ,
-        choices: [],
-    }
-    {
-        question: ,
-        answer: ,
-        choices: [],
-    }
-
+        question: "Where was Yun born?",
+        answer: "Seoul",
+        choices: ["Seoul", "Seattle", "Los Angeles", "Tokyo"],
+    },
 ];
+
+function showQuestion(questions) {
+    let questionNumber = 0;
+    let numberOfQuestions = questions.length;
+
+    question.textContent = `${questionNumber + 1}) ${questions[questionNumber].question}`;
+    questions[questionNumber].choices.forEach(choice => {
+        const choiceLI = document.createElement("li");
+        const choiceText = document.createTextNode(`${choice}`);
+        choiceLI.append(choiceText);
+        choicesUL.appendChild(choiceLI);
+    });
+
+    // console.log("question choices: ", questions[questionNumber].choices);
+    questionNumber++;
+}
 
 function startQuiz() {
     startSection.classList.add("disappear");
     gameSection.classList.remove("disappear");
+    showQuestion(questions);
 }
 
 startButton.addEventListener('click', startQuiz);
