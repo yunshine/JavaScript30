9f0e85f1118c6c565e1afe27cc927a7a15b2d7e7
Yun Chung <yun.chung@mac.com>
2021-03-06 11:44:04 +0900

    Notes on parsing and writing JSON

--- Asynchronous JavaScript Tutorial/app.js
+++ Asynchronous JavaScript Tutorial/app.js
@@ -13,7 +13,11 @@ const getTodos = (callbackFunction) => {
         // so, we also need to check that there weren't any errors with the request (status 200). Only then will there be a responseText/data...
         if (request.readyState === 4 && request.status === 200) {
             // Again, request.responseText is the data that you've gotten back once the request state/phase is at 4/done...
-            callbackFunction(undefined, request.responseText);
+            // callbackFunction(undefined, request.responseText);
+
+            // dealing with JSON...
+            const data = JSON.parse(request.responseText);
+            callbackFunction(undefined, data);
         } else if (request.readyState === 4) {
             callbackFunction('Error. Could not fetch data...', undefined);
         }
@@ -21,7 +25,10 @@ const getTodos = (callbackFunction) => {
     // How to send a request to get data...
     // ==================================================================
     // request.open tells our request what type of request it is (GET) and where to get that data from/where to send the request...
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todossss/');
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+
+    // accessing my own JSON file...
+    request.open('GET', 'todos.json');
     // request.open sets up the request, and request.send sends the request...
     request.send();
 };
