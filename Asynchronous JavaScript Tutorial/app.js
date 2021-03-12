099c0a5b7ee705d1816ecea53c524c353cda0207
Yun Chung <yun.chung@mac.com>
2021-03-12 12:04:39 +0900

    Added notes for throwing custom error messages

--- Asynchronous JavaScript Tutorial/app.js
+++ Asynchronous JavaScript Tutorial/app.js
@@ -160,9 +160,14 @@ fetch('json/todos.json').then(response => {
 const getTodosWithAsyncAwait = async () => {
     // once the fetch Promise below is resolved, JavaScript will then assign the data as the value to 'response'... this data will be a response object...
     const response = await fetch('json/todos.json');
-    const data = await response.json();
-
     console.log("The 'response' variable from getTodosWithAsyncAwait: ", response);
+
+    if (response.status !== 200) {
+        // when we throw a new Error in an async function, the returned Promise is 'rejected', not 'resolved'...
+        throw new Error('new Error thrown...');
+    }
+
+    const data = await response.json();
     console.log("The 'data' variable from getTodosWithAsyncAwait: ", data);
 
     return data;
@@ -172,7 +177,8 @@ console.log("a");
 console.log("b");
 
 getTodosWithAsyncAwait()
-    .then(data => console.log("Resolved data from getTodosWithAsyncAwait: ", data));
+    .then(data => console.log("Resolved data from getTodosWithAsyncAwait: ", data))
+    .catch(err => console.log("Err from getTodosWithAsyncAwait: ", err.message));
 
 console.log("c");
 console.log("d");
